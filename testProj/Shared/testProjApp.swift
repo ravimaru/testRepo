893241e11e34bd2f6f3ca04b3686879be04173b2
ravimaru <ravi.maru@indianic.com>
2021-11-09 15:23:28 +0530

    initial commit.

---
+++ testProj/Shared/testProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjApp.swift
+//  Shared
+//
+//  Created by indianic on 09/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
